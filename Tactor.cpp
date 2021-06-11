cf04a23185960d7e33e9195b994772f28ec345c6
Akshay Baweja <akshaybaweja.1996@gmail.com>
2021-06-11 04:29:11 -0400

    Initial Commit

---
+++ Tactor.cpp
@@ -0,0 +1,177 @@
+#include "Tactor.h"
+#include <math.h>
+
+#define DEBUG 0
+
+#if (DEBUG > 0)
+#define debug_update() { \
+    Serial.print("update: currPos:"); Serial.print(currPos);        \
+    Serial.print(" startPos:"); Serial.print(startPos);             \
+    Serial.print(" deltaPos:"); Serial.print(deltaPos);           \
+    Serial.print(" startMillis:"); Serial.print(startMillis);                     \
+    Serial.print(" arrived:");Serial.println(((arrived)?"true":"false"));}
+#define debug_getnextpos() { \
+    Serial.print("getNextPos:"); Serial.print(movesIndex); \
+    Serial.print(" deltaPos:"); Serial.print(deltaPos);     \
+    Serial.print(" durMillis:"); Serial.print(durMillis); \
+    Serial.print(" startMillis:"); Serial.println(startMillis); }
+#define debug_reset() { \
+    Serial.print("reset: currPos:"); Serial.print(currPos); \
+    Serial.print(" deltaPos:"); Serial.print(deltaPos); \
+    Serial.print(" durMillis:"); Serial.print(durMillis); \
+    Serial.print(" startMillis:"); Serial.println(startMillis); }
+#else
+#define debug_update() {}
+#define debug_getnextpos() {}
+#define debug_reset() {}
+#endif
+
+void Tactor::begin(Servo& s, int frameTime, int indicator)
+{
+  servo = &s;
+  frameMillis = frameTime;
+
+  direction = false;
+  arrived = true;
+
+  statusPin = indicator;
+  pinMode(statusPin, OUTPUT);
+
+  reset();
+}
+
+void Tactor::activate()
+{
+  active = true;
+  digitalWrite(statusPin, HIGH);
+}
+
+void Tactor::deactivate()
+{
+  active = false;
+  digitalWrite(statusPin, LOW);
+}
+
+void Tactor::reset()
+{
+  if (servo) {
+    currPos = servo->read();
+  }
+
+  startPos = currPos; // get everyone in sync;
+  deltaPos = 0;
+
+  startMillis = millis();
+
+  debug_reset();
+}
+
+void Tactor::moveTo(int pos, int dur)
+{
+  if (active) {
+    startPos = currPos;
+    deltaPos = pos - startPos;
+    durMillis = dur;
+    startMillis = millis();
+    arrived = false;
+  }
+}
+
+void Tactor::update()
+{
+  if (active) {
+    unsigned long currentMillis = millis();
+
+    if ( ((currentMillis - lastMillis) < frameMillis) || !running ) { //time yet?
+      return;
+    }
+
+    lastMillis = currentMillis;
+
+    if (currentMillis > (startMillis + durMillis)) {
+      currPos = startPos + deltaPos;
+    } else {
+      currPos = servoFunc(currentMillis - startMillis, startPos, deltaPos, durMillis);
+    }
+
+    debug_update();
+
+    float p = (direction) ? 180.0 - currPos : currPos;
+    if (servo && active)
+      if (useMicros)
+        servo->writeMicroseconds(angleToMicros(p));
+      else
+        servo->write(p);
+  }
+}
+
+void Tactor::setMinMaxMicroseconds(int mi, int ma)
+{
+  min = mi;
+  max = ma;
+  useMicros = true;
+}
+
+// from Servo.cpp
+#define SERVO_MIN() (MIN_PULSE_WIDTH - this->min * 4)  // min value in uS 
+#define SERVO_MAX() (MAX_PULSE_WIDTH - this->max * 4)  // max value in uS
+
+#define in_min 0
+#define in_max 180
+#define out_min SERVO_MIN()
+#define out_max SERVO_MAX()
+int Tactor::angleToMicros(float angle)
+{
+  //                x   in_min,in_max, out_min, out_max
+  //int value = map(value, 0, 180, SERVO_MIN(),  SERVO_MAX());
+  return (angle - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void Tactor::useMicroseconds(boolean t)
+{
+  useMicros = t;
+}
+
+boolean Tactor::usingMicroseconds()
+{
+  return useMicros;
+}
+
+void Tactor::setDirection(boolean dir)
+{
+  direction = dir;
+}
+
+boolean Tactor::getDirection()
+{
+  return direction;
+}
+
+void Tactor::start()
+{
+  if (!running)
+  {
+    startMillis += millis() - lastMillis;
+    running = true;
+  }
+}
+
+void Tactor::stop()
+{
+  running = false;
+}
+
+boolean Tactor::isRunning()
+{
+  return running;
+}
+
+boolean Tactor::hasArrived()
+{
+  return arrived;
+}
+
+float Tactor::getCurrPos()
+{
+  return currPos;
+}
---
+++ Tactor.h
@@ -0,0 +1,79 @@
+#ifndef Tactor_h
+#define Tactor_h
+
+#if (ARDUINO >= 100)
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#endif
+
+#include <Servo.h>
+
+typedef struct _servoMove {
+  int pos;
+  int dur;
+} ServoMove;
+
+// t:current time, b:beginning value, c:change in value, d: duration
+// t can be in frames or seconds/milliseconds
+typedef float (*ServoFunc)(float t, float b, float c, float d);
+
+class Tactor {
+  private:
+    Servo *servo;
+    int frameMillis;
+
+    float startPos;
+    float currPos;
+    float deltaPos;
+
+    unsigned long startMillis;
+    unsigned long durMillis;
+
+    unsigned long lastMillis;
+
+    boolean running;
+    boolean arrived;
+    boolean direction;
+
+    boolean useMicros;
+
+    int statusPin;
+    boolean active;
+
+    int angleToMicros(float angle);
+
+    uint8_t min;
+    uint8_t max;
+
+  public:
+    void begin(Servo& s, int frameTime, int indicator);
+    void reset();
+
+    void activate();
+    void deactivate();
+
+    void moveTo(int pos, int durMillis);
+
+    float getCurrPos();
+
+    void update();
+
+    void setServoFunc(ServoFunc);
+
+    boolean hasArrived();
+    boolean isRunning();
+
+    void start();
+    void stop();
+
+    void setDirection(boolean dir);
+    boolean getDirection();
+
+    void setMinMaxMicroseconds(int mi, int ma);
+
+    void useMicroseconds(boolean t);
+    boolean usingMicroseconds();
+}
+
+#endif
